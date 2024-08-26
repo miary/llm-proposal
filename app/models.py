2babf45b454837403e6aec0e6687c73d65cf3a62
Miary Andria <themia@gmail.com>
2024-08-26 05:34:11 -0700

    upload generator

--- app/models.py
+++ app/models.py
@@ -2,5 +2,11 @@ from upload_to import UploadTo
 from django.db import models
 
 
+def my_upload_generator(instance, filename):
+    filename = UploadTo.uuid_filename(filename)
+    path = UploadTo.options_from_instance(instance)
+    return UploadTo.upload_to(path, filename)
+
+
 class AttachModel(models.Model):
     attachment = models.FileField(upload_to=UploadTo("media/attachments"))
